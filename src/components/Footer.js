97c449082d89dcbd42a526b482cdff182a7cb344
mayank160920 <ay160920@gmail.com>
2021-10-22 13:24:16 -0400

    fixed news ui bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="footer">
-            <p>Made With ❤️ by <span style={{color : '#fd7014'}}>Mayank</span></p>
+            <p>Made With ❤️ by <span style={{color : '#fd7014'}} href="https://github.com/mayank160920">Mayank</span></p>
         </div>
     )
 }
