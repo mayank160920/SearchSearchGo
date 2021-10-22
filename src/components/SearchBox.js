3a871a81461e6aa0813b9fa005960b337c408bd9
sduedyetugiuiag <sduedyetugiuiag@pussport.com>
2021-10-22 12:16:27 -0400

    fixed input border

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { AiOutlineSearch } from 'react-icons/ai';
 
-import {useResultsContext} from '../context/ResultsContextProvider';
+import { useResultsContext } from '../context/ResultsContextProvider';
 
 function SearchBox() {
-    const [input,setInput] = React.useState("");
-    const {setSearchTerm} = useResultsContext();
+    const [input, setInput] = React.useState("");
+    const { setSearchTerm } = useResultsContext();
 
     return (
         <div className="searchBox">
-            <input 
-                className="searchInput" 
-                type="text" 
-                value={input} 
-                onChange={(e) => setInput(e.target.value)} 
+            <input
+                className="searchInput"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyPress={(e) => { if (e.key === "Enter") { setSearchTerm(input) } }}
                 autoFocus />
-            <div 
-                className="searchIcon-wrapper" 
+            <div
+                className="searchIcon-wrapper"
                 onClick={() => setSearchTerm(input)}>
                 <AiOutlineSearch className="searchIcon" />
             </div>
